59b231932dddd628017cdb3aa48da03e6ce5692e
Osama Ammar <ossssamar@gmail.com>
2022-10-20 09:25:26 +0200

    adding leet code comments

--- memory_managment.cpp
+++ memory_managment.cpp
@@ -9,7 +9,7 @@ int main(){
 //nt *p= new int;   //by this expression we request (book) a memory location using pointer and new
 int *p=new int(3);              //we store a value in this location
 cout<<*p<<endl;    // we use this value
-delete p ;         //freeing up (heap) memory from this value using delete , buy the pointer still not deleted ,So pointer is left pointing to non existing memoty location(dangling pointer)      
+delete p ;         //freeing up (heap) memory from this value using delete , but the pointer still not deleted ,So pointer is left pointing to non existing memoty location(dangling pointer)      
 
 p=new int;          //reusing p for a new address
 
@@ -18,6 +18,30 @@ nptr=new int[15];     //requesting (booking) a memory for an array
 delete [] p;          //deleting that array
 
 cout<<sizeof(long int)<<endl; //give you the size of specific data type in your computer ..according to your coputer system and compiler used
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	// allocate memory for 20 integers (80 bytes)
+	int * array = new int[20];
+
+	cout << "The address of 'array' is " << &array << endl;
+	cout << "The value of 'array' is " << *array << endl;
+
+	for ( int i = 0; i < 20; ++i ) {
+		cout << "The address of array[" << i << "] is " << &( array[i] ) << endl;
+	}
+
+	// must clean it up
+	delete[] array;
+	array = 0;
+
+
+
+
+
+
+
+
+
 }
 
 
