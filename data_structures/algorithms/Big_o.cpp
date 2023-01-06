2a4b56a2b039d6911830bcef956b0065b7c432c3
Osama Ammar <ossssamar@gmail.com>
2023-01-06 15:05:53 +0200

    exploring sorting algorithms

--- data_structures/algorithms/Big_o.cpp
+++ data_structures/algorithms/Big_o.cpp
@@ -13,6 +13,7 @@ There are mainly three asymptotic notations:
     Theta notation : Theta notation encloses the function from above and below. Since it represents the upper and the lower bound of the running time of an algorithm,
                     it is used for analyzing the average-case complexity of an algorithm.
 
+    ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
     ?? The master theorem is used in calculating the time complexity of recurrence relations (divide and conquer algorithms) in a simple and quick way.
     T(n) = aT(n/b) + f(n),
@@ -22,23 +23,44 @@ There are mainly three asymptotic notations:
     n/b = size of each subproblem. All subproblems are assumed 
         to have the same size.
     f(n) = cost of the work done outside the recursive call, 
-        which includes the cost of dividing the problem and
-        cost of merging the solutions
+            which includes the cost of dividing the problem and
+            cost of merging the solutions
 
     Here, a ≥ 1 and b > 1 are constants, and f(n) is an asymptotically positive function.
 
 
-    example: consider you have an array of 12 elements and you want to sort it ....and you will use devide and conquer algorithm to do this
-                we will divide the arry into 2 arrays and divide each subarray will although be divided into 2 subarrays and so on 
+    example: consider you have an array of 12 elements and you want to sort it ....and you will use merge sort algorithm that depends devide and conquer algorithm (It makes efficient use of memory caches.)
+    to do this
+    we will divide the arry into 2 arrays and divide each subarray will although be divided into 2 subarrays and so on 
+
+    ------------------------------------------------------------------------------------------------------------------------------
+    in our example :
+    T(n) = aT(n/b) + f(n)
+        = 2T(n/2) + O(n)
+    Where, 
+    a = 2 (each time, a problem is divided into 2 subproblems)
+    n/b = n/2 (size of each sub problem is half of the input)
+    f(n) = time taken to divide the problem and merging the subproblems
+    T(n/2) = O(n log n) (To understand this, please refer to the master theorem.)
+
+    Now, T(n) = 2T(n log n) + O(n)
+            ≈ O(n log n)
 
 
-    let's use this formula
-    T(n) = aT(n/b) + f(n),
-    where,
-    n = size of input
-    a = number of subproblems in the recursion
-    n/b = size of each subproblem. All subproblems are assumed to have the same size.
-    f(n) = cost of the work done outside the recursive call, which includes the cost of dividing the problem and cost of merging the solutions
+
+    ----------------------------------------------------------------------------------------------------------------------------------------------
+
+    --------------------------------------------------------------------------
+    The divide and conquer approach divides a problem into smaller subproblems; these subproblems are further solved recursively. 
+    The result of each subproblem is not stored for future reference .whereas,in a dynamic approach, the result of each subproblem is stored for future reference.
+    so , In a dynamic approach we result of each subproblem.
+
+
+
+
+
+
+
 
 
 */
