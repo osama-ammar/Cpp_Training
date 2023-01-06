fd92a781be463e8170d463ec61cbb841524251ae
Osama Ammar <ossssamar@gmail.com>
2023-01-06 17:53:24 +0200

    getting a working code

--- data_structures/algorithms/sorting.cpp
+++ data_structures/algorithms/sorting.cpp
@@ -1,5 +1,5 @@
 /*
-    sorting algorithms 
+    sorting algorithms
     -------------------
 
 
@@ -7,11 +7,6 @@
 
 
 */
-
-
-
-
-
 
 // Bubble sort in C++ --------------------------------------------------------------------------------------------
 // avg Time complexity = O(n2)
@@ -21,135 +16,273 @@
 using namespace std;
 
 // perform bubble sort
-void bubbleSort(int array[], int size) {
+void bubbleSort(int array[], int size)
+{
 
-  // loop to access each array element
-  for (int step = 0; step < size; ++step) {
-      
-    // loop to compare array elements
-    for (int i = 0; i < size - step; ++i) {
+    // loop to access each array element
+    for (int step = 0; step < size; ++step)
+    {
 
-      // compare two adjacent elements
-      // change > to < to sort in descending order
-      if (array[i] > array[i + 1]) {
+        // loop to compare array elements
+        for (int i = 0; i < size - step; ++i)
+        {
 
-        // swapping elements if elements
-        // are not in the intended order
-        int temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
-      }
+            // compare two adjacent elements
+            // change > to < to sort in descending order
+            if (array[i] > array[i + 1])
+            {
+
+                // swapping elements if elements
+                // are not in the intended order
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
     }
-  }
 }
-
-// print array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; ++i) {
-    cout << "  " << array[i];
-  }
-  cout << "\n";
-}
-
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Optimized bubble sort in C++
+/*we can introduce an extra variable swapped. The value of swapped is set true if there occurs swapping of elements. Otherwise, it is set false.
+
+After an iteration, if there is no swapping, the value of swapped will be false. This means elements are already sorted and there is no need to perform further iterations.
+
+This will reduce the execution time and helps to optimize the bubble sort.*/
 // avg Time complexity = O(n2)
 // Space complexity = O(2)
 //
 // perform bubble sort
-void bubbleSort(int array[], int size) {
-    
-  // loop to access each array element
-  for (int step = 0; step < (size-1); ++step) {
-      
-    // check if swapping occurs
-    int swapped = 0;
-    
-    // loop to compare two elements
-    for (int i = 0; i < (size-step-1); ++i) {
-        
-      // compare two array elements
-      // change > to < to sort in descending order
-      if (array[i] > array[i + 1]) {
+void optimized_bubbleSort(int array[], int size)
+{
 
-        // swapping occurs if elements
-        // are not in intended order 
-        int temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
-        
-        swapped = 1;
-      }
+    // loop to access each array element
+    for (int step = 0; step < (size - 1); ++step)
+    {
+
+        // check if swapping occurs
+        int swapped = 0;
+
+        // loop to compare two elements
+        for (int i = 0; i < (size - step - 1); ++i)
+        {
+
+            // compare two array elements
+            // change > to < to sort in descending order
+            if (array[i] > array[i + 1])
+            {
+
+                // swapping occurs if elements
+                // are not in intended order
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+
+                swapped = 1;
+            }
+        }
+
+        // no swapping means the array is already sorted
+        // so no need of further comparison
+        if (swapped == 0)
+            break;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Selection sort in C++
+/*election sort is a sorting algorithm that selects the smallest element from an unsorted list in each iteration and places that element at the beginning of the unsorted list.*/
+
+//  Time complexity = O(n2)
+// Space complexity = O(1)
+
+#include <iostream>
+using namespace std;
+
+// function to swap the the position of two elements
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int array[], int size)
+{
+    for (int step = 0; step < size - 1; step++)
+    {
+        int min_idx = step;
+        for (int i = step + 1; i < size; i++)
+        {
+
+            // To sort in descending order, change > to < in this line.
+            // Select the minimum element in each loop.
+            if (array[i] < array[min_idx])
+                min_idx = i;
+        }
+
+        // put min at the correct position
+        swap(&array[min_idx], &array[step]);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Merge two subarrays L and M into arr void merge(int arr[], int p, int q, int r)
+//  Time complexity = O(n*log n)
+// Space complexity = O(n)
+
+// Merge two subarrays L and M into arr
+void merge(int arr[], int p, int q, int r)
+{
+
+    // Create L ← A[p..q] and M ← A[q+1..r]
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    // Maintain current index of sub-arrays and main array
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    // Until we reach either end of either L or M, pick larger among
+    // elements L and M and place them in the correct position at A[p..r]
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
     }
 
-    // no swapping means the array is already sorted
-    // so no need of further comparison
-    if (swapped == 0)
-      break;
-  }
+    // When we run out of elements in either L or M,
+    // pick up the remaining elements and put in A[p..r]
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
 }
 
-// print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; ++i) {
-    cout << "  " << array[i];
-  }
-  cout << "\n";
+// Divide the array into two subarrays, sort them and merge them
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // m is the point where the array is divided into two subarrays
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the sorted subarrays
+        merge(arr, l, m, r);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Quicksort
+// is a sorting algorithm based on the divide and conquer
+//  Time complexity = O(n2)
+// Space complexity = O(log n)
+
+// function to rearrange array (find the partition point)
+int partition(int array[], int low, int high)
+{
+
+    // select the rightmost element as pivot
+    int pivot = array[high];
+
+    // pointer for greater element
+    int i = (low - 1);
+
+    // traverse each element of the array
+    // compare them with the pivot
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+
+            // if element smaller than pivot is found
+            // swap it with the greater element pointed by i
+            i++;
+
+            // swap element at i with element at j
+            swap(&array[i], &array[j]);
+        }
+    }
+
+    // swap pivot with the greater element at i
+    swap(&array[i + 1], &array[high]);
+
+    // return the partition point
+    return (i + 1);
+}
+
+void quickSort(int array[], int low, int high)
+{
+    if (low < high)
+    {
+
+        // find the pivot element such that
+        // elements smaller than pivot are on left of pivot
+        // elements greater than pivot are on righ of pivot
+        int pi = partition(array, low, high);
+
+        // recursive call on the left of pivot
+        quickSort(array, low, pi - 1);
+
+        // recursive call on the right of pivot
+        quickSort(array, pi + 1, high);
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Print the array
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 ////////////////////////////////////////////////
-int main() {
-  int data[] = {-2, 45, 0, 11, -9};
-  
-  // find the array's length
-  int size = sizeof(data) / sizeof(data[0]);
-  
-  bubbleSort(data, size);
-  
-  cout << "Sorted Array in Ascending Order:\n";
-  printArray(data, size);
+int main()
+{
+    int data[] = {-2, 45, 0, 11, -9};
+
+    // find the array's length
+    int size = sizeof(data) / sizeof(data[0]);
+
+    bubbleSort(data, size);
+
+    cout << "Sorted Array in Ascending Order:\n";
+    printArray(data, size);
 }
 /////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
