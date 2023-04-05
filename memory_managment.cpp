de5394603325c0f1ed0044af2ee50f4292bc8af1
Osama Ammar <ossssamar@gmail.com>
2023-04-05 10:49:20 +0200

    memory management

--- memory_managment.cpp
+++ memory_managment.cpp
@@ -1,26 +1,43 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
 // there are two types of memory stack & heap ; Stack:managing memory on it is automatic , store local variables ,
 //												heap::managing memory on it is manual , unused program memory that can be used when the program runs dynamically
 // we cannot control life time in stack but we can do this in heap using new and delete operators.
 
 
 
-#include <iostream>
-#include <string>
-using namespace std;
 int main(){
 
 
-int stackvar= 3;   //this lives in  stack 
-int *ptrheap=new int(3);  // this lives in heap
 //////////////////////////////////////////////////////////////////
-// best practice
+//                 Stack
+//////////////////////////////////////////////////////////////////
 
-int* ptr_stackvar2 = new int(77);
-//ptr_stackvar2 =& stackvar; 
+int stackvar= 3;   //this lives in  stack 
+int cat[]={2,33,442,43,33,22};
+int var=4;
 
-delete ptr_stackvar2;
-ptr_stackvar2 = nullptr;
-/////////////////////////////////////////////////////////////////////////////
+int* kom =nullptr; //initialize a pointer that does not point to anything 
+					//the correct way of  initializing a new pointer
+cout << "kom = " << kom << endl;
+//cout << "*kom = " << *kom << endl;
+cout << "&kom = " << &kom << endl;
+
+int * ptrvar= &var;
+// int *novar=var;   error ---not posssible (int* is considered a data type that only accept memory address)
+cout << "ptrvar = " << ptrvar << endl;
+cout << "*ptrvar = " << *ptrvar << endl;
+printf("my name is %d ,%s",4,"osama");
+
+//////////////////////////////////////////////////////////////////
+//                 Heap
+//////////////////////////////////////////////////////////////////
+
 
 //int *p= new int;   //by this expression we request (book) a memory location in heap using pointer and new
 int *p=new int(3);              //we store a value in this location
@@ -33,30 +50,24 @@ int *nptr= NULL;     //pointer of value of zero and still have no exact address 
 nptr=new int[15];     //requesting (booking) a memory for an array  
 delete [] p;          //deleting that array
 
-cout<<sizeof(long int)<<endl; //give you the size of specific data type in your computer ..according to your coputer system and compiler used
+//cout<<sizeof(long int)<<endl; //give you the size of specific data type in your computer ..according to your coputer system and compiler used
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-	// allocate memory for 20 integers (80 bytes)
-	int * array = new int[20];
+//////////////////////////////////////////////////////////////////////////////////
+//   							Arryas
+/////////////////////////////////////////////////////////////////////////////////
 
-	cout << "The address of 'array' is " << &array << endl; /// every time you run this line we will get different results
-	cout << "The value of 'array' is " << *array << endl;
+// allocate memory for 20 integers (80 bytes)
+int * array = new int[20];
+//cout << "The address of 'array' is " << &array << endl; /// every time you run this line we will get different results
+//cout << "The value of 'array' is " << *array << endl;
+for ( int i = 0; i < 20; ++i ) {
+	// cout << "The address of array[" << i << "] is " << &( array[i] ) << endl;
+}
 
-	for ( int i = 0; i < 20; ++i ) {
-		// cout << "The address of array[" << i << "] is " << &( array[i] ) << endl;
-	}
-
-	// must clean it up
-	delete[] array;
-	array = 0;
-
-
-
-
-
-
-
-
+// must clean it up
+delete[] array;
+array = 0;
+	return 0;
 
 }
 
