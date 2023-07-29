ba9def8b1e1930c740aacc0ab2f88ace7cfb7ddc
Osama Ammar <ossssamar@gmail.com>
2023-07-29 14:00:05 +0300

    formatting

--- data_structures/structure.cpp
+++ data_structures/structure.cpp
@@ -1,47 +1,39 @@
 
-// structure is a user defined data type tha tcan contain differet data types
+// structure is a user defined data type that can contain differet data types
 // union looks like exactly the structure ..but...it stores only one member value which is the largest value
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-
-struct Test{
+struct Test
+{
     string name;
-    int number; 
+    int number;
     int degree;
-
 };
 
-union Union_Test{
+union Union_Test
+{
     string name;
-    int number; 
+    int number;
     int degree;
-
 };
 
-
-
-
-
-
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
     // store a value (using pointers) that has not variable name
-    int* ptr=new int(20); // this pointer points to value 20 ...this value has no name ( name of variable)...but we know the address of this value
-    cout<<"ptr: "<<ptr<<endl;
-    cout<<"*ptr: "<<*ptr<<endl;
+    int *ptr = new int(20); // this pointer points to value 20 ...this value has no name ( name of variable)...but we know the address of this value
+    cout << "ptr: " << ptr << endl;
+    cout << "*ptr: " << *ptr << endl;
     cout << "------------------" << endl;
-
 
     Test lolo;
     lolo.name = "abuza";
     lolo.number = 1564;
     cout << lolo.name << endl;
     cout << "------------------" << endl;
-
-
 
     Test *popo;
     popo->name = "abuza";
@@ -50,9 +42,7 @@ int main(int argc, char** argv){
     cout << popo->name << endl;
     cout << "------------------" << endl;
 
-
-
-    Test koko={"OSAMA",1481,97};
+    Test koko = {"OSAMA", 1481, 97};
     cout << koko.number << endl;
     cout << "------------------" << endl;
 
@@ -68,44 +58,18 @@ int main(int argc, char** argv){
     cout << books[1].number << endl;
     cout << "------------------" << endl;
 
+    /*
+        Union_Test uniko;
+        uniko.name = "yoko";
+        uniko.number = 6434;
+        uniko.degree = 12;
 
 
-
-
-
-
-
-/*
-    Union_Test uniko;
-    uniko.name = "yoko";
-    uniko.number = 6434;
-    uniko.degree = 12;
-
-
-    cout << uniko.name << endl;
-    cout << uniko.number << endl;
-    cout << uniko.degree << endl;
-    cout << "------------------" << endl;
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
+        cout << uniko.name << endl;
+        cout << uniko.number << endl;
+        cout << uniko.degree << endl;
+        cout << "------------------" << endl;
+    */
 
     return 0;
 };
-
-
-
-
-
-
