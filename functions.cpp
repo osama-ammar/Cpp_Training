44d4df9226a81a139678520cba14d5ea45273d09
Osama Ammar <ossssamar@gmail.com>
2023-12-10 09:37:18 +0200

    making functions more clear

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include <cstdlib>   // to take random function from it
+#include <cstdlib> // to take random function from it
 using namespace std;
-
 
 /*
 E-difference between overloading and overriding?
@@ -13,80 +12,88 @@ E-difference between overloading and overriding?
 				-compile time polymorphism
 				-can be done in one class
 
--overriding : redifing a function \method with the same name and parameters 
+-overriding : redifing a function \method with the same name and parameters
 				usually occuers in class inhertince , when child class override a parent method
-				-run time polymorphism 
+				-run time polymorphism
 				-should be done in 2 classes
 */
 
-
-
-
-
-
-
-void fun0(int a,int b,int c)   //by this method (assigning the arguments by value  we cannot make modification to the parameter ,we just take a copy of the variable and use it as an argument
-	{
-		int sum=a+b+c;
-		cout<<sum<<endl;	
-	}
-	
-void fun_ref(int *a) //by this method (assigning the arguments by reference -not value- we can make modification to the parameter, we take a copy of variable address and use it to access the variable itself in the memory..this saves memory rather than copying very large variable sizes
-	{
-		
-		 * a=70;
-		 cout<<*a<<endl;
-		 	
-	}
-
-void fun_ref(int &a) //?????????????????????
-	{
-		
-		a=70;
-		cout<<a<<endl;
-	}
-	
-int fun (float a,float b,float c) {  // this is called function overloading to make the same function valid for many types of inputs 
-		float sum=a+b+c;
-		cout<<sum<<endl;	
+void fun0(int a, int b, int c) // by this method (assigning the arguments by value  we cannot make modification to the parameter ,we just take a copy of the variable and use it as an argument
+{
+	int sum = a + b + c;
+	cout << sum << endl;
 }
-	
-int factorial (int n){					//this is call recursion : making a function recall itself (figures for recursion and memory in c++)
-		if (n==1){					// the exit condition (base case) important in recursion 
-			return 1;
-		}
-		
-		else {
-			return n*factorial(n-1);
-		}
 
+/////////////////////////////////////////////////////////////////
+/*
+calling by ref , most used method
+
+*/
+void fun_ref(int & ref) 
+{
+
+	ref += 70;
+	cout << ref << endl;
+}
+
+/*
+calling by pointer , used when dealing with dynamic memory allocation
+befor using it , you should make a pointer of the var
+*/
+void fun_pointer(int * pointer)
+{
+
+	*pointer += 70;
+	cout << *pointer << endl;
+}
+//////////////////////////////////////////////////////////////////
+
+int fun(float a, float b, float c)
+{ // this is called function overloading to make the same function valid for many types of inputs
+	float sum = a + b + c;
+	cout << sum << endl;
+}
+
+int factorial(int n)
+{ // this is call recursion : making a function recall itself (figures for recursion and memory in c++)
+	if (n == 1)
+	{ // the exit condition (base case) important in recursion
+		return 1;
+	}
+
+	else
+	{
+		return n * factorial(n - 1);
+	}
 };
 
 /*void print_arr (int arr[],int size){	       //argument here is an array
 		for (int i;x<size;x++){
-			cout<<arr[i]<<endl;		
+			cout<<arr[i]<<endl;
 		}
 	}*/
-	
-int main(){
-	
-	//srand(34) //if used  we will obtain a true random number tht will be produced every time you used the same seed
-	//srand(time(0)) //if used  we will guarantee obtaining a true random number using the current time as a seed 
-	// for (int i ;i<8;i++){
-	// 	cout<<1+(rand()%10)<<endl; //psudo random number between 1 and 10
-	// }	
-		
-	int var=3;
-	fun_ref(&var);
-	cout<<var<<endl;
-	
-	//fun(1,2,3);
-	//fun(1.2,2.8,4.5);
-	
-	
+
+int main()
+{
+
+	// srand(34) //if used  we will obtain a true random number tht will be produced every time you used the same seed
+	// srand(time(0)) //if used  we will guarantee obtaining a true random number using the current time as a seed
+	//  for (int i ;i<8;i++){
+	//  	cout<<1+(rand()%10)<<endl; //psudo random number between 1 and 10
+	//  }
+
+	int var = 3;
+	fun_ref(var);
+	cout << var << endl;
+
+
+	int kom =8;
+	int *kom_ptr = &kom ;
+	fun_pointer(kom_ptr);
+	cout << kom << endl;
+
+	// fun(1,2,3);
+	// fun(1.2,2.8,4.5);
+
 	return 0;
 };
-
-
-
-
